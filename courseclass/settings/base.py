4fcd2db0c213780f1fe6152836f1b8fafb0617c9
tsurantino <arturtsurkan@gmail.com>
2014-01-18 14:48:22 -0500

    Added registration, thumbnails packages. Added three core apps which should cover most for this project.

--- courseclass/settings/base.py
+++ courseclass/settings/base.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'south',
+    'registration',
+    'easy_thumbnails',
 )
 
 MIDDLEWARE_CLASSES = (
--- courseclass/urls.py
+++ courseclass/urls.py
@@ -6,4 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
