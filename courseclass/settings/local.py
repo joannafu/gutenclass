0e0f5d19c6ff65f0e6be58d82a589b224eb9ef08
tsurantino <arturtsurkan@gmail.com>
2014-01-17 23:47:51 -0500

    Added the index page.

--- courseclass/settings/local.py
+++ courseclass/settings/local.py
@@ -1,1 +1,11 @@
-from base import *
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- courseclass/urls.py
+++ courseclass/urls.py
@@ -1,12 +1,9 @@
+from django.views.generic import TemplateView
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'courseclass.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
